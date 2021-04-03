546056f71b65c7d741ad292f1ee49aaab0b10cff
TheRealDxRed <ben.dumouchel@gmail.com>
2021-04-03 12:17:36 -0400

    added stage collider system removed tiled collisions

---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,6 @@
+public static class Globals {
+    public static int LAYER_TERRAIN = 8;
+    public static int LAYER_PLATFORM = 9;
+    public static int LAYER_WALL = 10;
+    public static int LAYER_CEILING = 11;
+}
