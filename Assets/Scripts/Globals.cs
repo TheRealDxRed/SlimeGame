b1821587ba650c40102babc46db7f84789da9e3d
TheRealDxRed <ben.dumouchel@gmail.com>
2021-04-09 18:47:55 -0400

    tilemap collisions are back, baby

--- Assets/Scripts/Globals.cs
+++
@@ -1,6 +0,0 @@
-public static class Globals {
-    public static int LAYER_TERRAIN = 8;
-    public static int LAYER_PLATFORM = 9;
-    public static int LAYER_WALL = 10;
-    public static int LAYER_CEILING = 11;
-}
